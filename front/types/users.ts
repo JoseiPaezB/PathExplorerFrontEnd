5d04f23aca12bce181b63e78d91b9095ecbbbb16
Gustavo Coutiño <gcoutino04@gmail.com>
2025-04-10 11:35:44 -0600

    Add estado banco in user list

---
+++ front/types/users.ts
@@ -0,0 +1,17 @@
+export interface UserInfoBanca {
+  id_persona: number;
+  nombre: string;
+  apellido: string;
+  email: string;
+  fecha_contratacion: string;
+  fecha_creacion: string;
+  fecha_actualizacion: string;
+  password_hash: string;
+  id_empleado: number;
+  estado: string;
+  porcentaje_disponibilidad: string;
+  id_perfil: number;
+  puesto_actual: string;
+  antiguedad: number;
+  historial_profesional: string;
+}
